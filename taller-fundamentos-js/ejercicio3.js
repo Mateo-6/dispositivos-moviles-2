c76d15063b82abc27e125bb8bd08967a91c95779
Mateo <mateoarango05@gmail.com>
2023-03-01 22:33:53 -0500

    remove unnecessary processes from exercise 3

--- taller-fundamentos-js/ejercicio3.js
+++ taller-fundamentos-js/ejercicio3.js
@@ -1,12 +1,5 @@
 const factorial = [1, 2, 3, 4, 5];
 
-let factorial2 = 5;
-for (let key of factorial) {
-  factorial2 = factorial2 * key;
-}
-
-console.log(factorial2);
-
 // 0 + 1 + 2 + 3 + 4
 const initialValue = 5;
 const sumWithInitial = factorial.reduce(
